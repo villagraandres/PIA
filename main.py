c57b52f1a6d9b081c348da4cb48400fa641374c9
andres <villagraandres00@gmail.com>
2024-04-20 13:04:00 -0600

    ideas

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
-print("a")
+import requests
+
+
+
+if __name__=="__main__":
+    print("menu")
+    #opciones
+    #buscar repositorio
+    #buscar usuario
+    #despues de buscar listar las coincidencias y elegir uno
+    """ en caso de repo listar estrellas // issues cerradas o abiertas
+    pull request o lenguajes de programacion"""
+
+    """en caso de usuario listar los repos, seguidores o descripcion o estrellas """
