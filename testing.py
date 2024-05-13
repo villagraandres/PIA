4196043af439991be67e02cb81d2f3929748a7ab
Amadeus <aleksisurena@gmail.com>
2024-05-12 23:45:03 -0600

    Ignora lo anterior

--- testing.py
+++
@@ -1,4 +0,0 @@
-programming_languages = ["Python", "JavaScript", "Java", "C++", "C#", "PHP", "Ruby", "Swift", "Go", "Rust"]
-indexex = [i for i in range(len(programming_languages)]
-t_entre_comm = dict(zip(programming_languages,indexex))
-print(t_entre_comm)
